fef194e20787acb6b41f852fdc2c5bd290536ada
Hayan <ryu@hangnya.com>
2021-09-10 17:57:30 +0900

    클론코딩

--- script/main.js
+++ script/main.js
@@ -1,5 +1,44 @@
 $(function(){
-    $(".ad_close").click(function(){
-        $(".ad").hide();
+    ad();
+    modal();
+    movieExplorer();
+    setInterval(mouseScroll, 1500);
+});
+function mouseScroll(){
+    $("#mouse").animate({
+        bottom: "-25"
+    },"swing",function(){
+        $("#mouse").animate({
+            bottom: "-5"
+        });
     })
-})
+}
+function movieExplorer(){
+    $("input[type=image]").click(function(){        
+        var keyword = $("input[type=text]").val();
+        if(!keyword) {
+            alert("영화명을 입력하셔야 합니다");
+        } else {
+            console.log(keyword);
+            location.href="https://www.megabox.co.kr/movie?searchText="+keyword;
+        }
+    });
+}
+function modal() {    
+
+    $(".favorite").click(function(){
+        $("#modal").show();
+    });
+
+    $("#close_x").click(function(){
+        $("#modal").hide();
+    });
+    $("#close_ok").click(function(){
+        $("#modal").hide();
+    });
+}
+function ad(){
+    $(".ad-close").click(function(){
+        $(".ad").hide();
+    });
+}
